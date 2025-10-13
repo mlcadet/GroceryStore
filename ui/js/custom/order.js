4bc574381e67391fec13542522f0da1e22557fb6
Lucie <cadet.ml@gmail.com>
2025-10-13 11:33:57 -0400

    order.js

--- ui/js/custom/order.js
+++ ui/js/custom/order.js
@@ -1,11 +1,77 @@
-import React from 'react'
+const productPrices = {};
 
-const order = () => {
-  return (
-    <div>
+    // JSON data by api for order table
+  $.get(productListApiUrl, function (response) {
+    if (response) {
+      let options = '<option value="">Select Product</option>';
+      $.each(response, function (index, product) {
+        options += `<option value="${product.product_id}" data-price="${product.price}">${product.name}</option>`;
+        productPrices[product.product_id] = product.price;
+      });
+      $(".product-box").find("select").empty().html(options);
+    }
+  });
+  
+    // Add new button click
+  $("#addMoreButton").click(function () {
+    const row = $(".product-box").first().clone();
+    row.find(".remove-row").removeClass("hideit");
+    row.find(".product-price").text("0.00");
+    row.find(".product-qty").val("1");
+    row.find(".product-total").text("0.00");
+    $(".product-container").append(row);
+  });
+
+    // Calculate grand total
+    $(document).on("click", ".remove-row", function () {
+      $(this).closest('.row').remove();
+      calculateGrandTotal();
+    });
+
+    // On product change
+    $(document).on("change", ".cart-product", function () {
+      const product_id = $(this).val();
+      const price = productPrices[product_id] || 0;
       
-    </div>
-  )
-}
+      $(this).closest('.row').find('.product-price').val(price);
+      calculateGrandTotal();
+    });
 
-export default order
+    // SAVE ORDER
+    $("#saveOrder").on("click", function () {
+    const formData = $(form).serializeArray();
+    const requestPayload = {
+      customer_name: null,
+      total: null,
+      order_details: []
+    };
+    let currentItem = null;
+
+    formData.forEach(element => {
+      switch (element.name) {
+        case 'customerName':
+          requestPayload.customer_name = element.value;
+          break;
+        case 'product_grand_total':
+          requestPayload.total = element.value;
+          break;
+        case 'product':
+          currentItem = { product_id: element.value };
+          requestPayload.order_details.push(currentItem);
+          break;
+        case 'qty':
+          if (currentItem) currentItem.quantity = element.value;
+          break;
+        case 'item_total':
+          if (currentItem) currentItem.total_price = element.value;
+          break;
+      }
+    });
+
+    callApi("POST", orderSaveApiUrl, {
+      data: JSON.stringify(requestPayload)
+    }, function(response) {
+      alert('Order saved successfully');
+    });
+}); 
+        
